6d4f4da7561483607c9cef15e5ee30fb7e32002a
Jatin <chaurasiyajatin68@gmail.com>
2023-12-30 12:43:38 +0530

    code 17

--- COMPUTER PRACTICAL EXPERIMENTS/exp_17.cpp
+++ COMPUTER PRACTICAL EXPERIMENTS/exp_17.cpp
@@ -7,23 +7,21 @@ class shape{
     public:
     void area(int);
     void area(int,int);
-    void area( float, float);
+    void area( double, double);
     
 };
-void shape ::void area(int r){
+void shape :: area(int r){
     float a = 3.141*r*r;
     cout<<"area of circle :"<<a<<endl;
-    return 0;
+
 }
-void shape ::void area(int l, int b){
+void shape :: area(int l, int b){
     int a1 = l*b;
-    cout<<"area of rectangle is :"<<a1;
-return 0;
+    cout<<"area of rectangle is :"<<a1<<endl;
 }
-void shape ::void area(float h, float f){
-    float a2 = 0.5*l*b;
-    cout<<"area of triangle is :"<<a2;
-return 0;
+void shape :: area(double h, double f){
+    double a2 = 0.5*h*f;
+    cout<<"area of triangle is :"<<a2<<endl;
 }
 
 int main(){
