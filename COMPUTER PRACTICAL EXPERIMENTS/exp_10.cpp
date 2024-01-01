86296fb619dedb0bb1ac25270346102c905ee5bd
Jatin <chaurasiyajatin68@gmail.com>
2024-01-01 17:38:02 +0530

    codes

--- COMPUTER PRACTICAL EXPERIMENTS/exp_10.cpp
+++ COMPUTER PRACTICAL EXPERIMENTS/exp_10.cpp
@@ -1,1 +1,24 @@
 //wap to implement a two dimentional array for reading and printing elements of a matrix
+#include <iostream> 
+using namespace std; 
+
+int main() { 
+    int arr[3][3];
+    
+    for (int i = 1; i < 4; i++) { 
+        for (int j = 1; j < 4; j++) { 
+            cout<<"enter value for row "<< i<<" column "<<j<<endl;
+            cin>>arr[i][j];
+        } 
+        cout << endl; 
+    } 
+
+    for (int i = 1; i < 4; i++) { 
+        for (int j = 1; j < 4; j++) { 
+            cout << arr[i][j] << " "; 
+        } 
+        cout << endl; 
+    } 
+
+    return 0; 
+} 
--- COMPUTER PRACTICAL EXPERIMENTS/exp_9.cpp
+++ COMPUTER PRACTICAL EXPERIMENTS/exp_9.cpp
@@ -4,18 +4,24 @@ wap to illustrate an one dimentional array of searching an element present in an
 */
 #include<iostream>
 using namespace std;
-
-int main(){
-    int i,A[10],s=0;
-    cout<<"enter 10 numbers:"<<endl;
-    for (int i = 0; i < 10; i++)
-    {
-        cin>>A[i];
-        s =s+A[i];
-
-    }
-    cout<<"sum of  the elements present in given array is :"<<s<<endl;
-
+int main()
+{
     
+    cout<<"Enter 10 Numbers: "<<endl;
+    int arr[10], i, num, index;
+    for(i=0; i<10; i++){
+        cin>>arr[i];}
+    cout<<"Enter a Number to Search: "<<endl;
+    cin>>num;
+    for(i=0; i<10; i++)
+    {
+        if(arr[i]==num)
+        {
+            index = i;
+            break;
+        }
+    }
+    cout<<"Found at Index No."<<index+1<<endl;
+    cout<<endl;
     return 0;
 }
