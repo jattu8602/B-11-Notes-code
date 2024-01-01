a48d72466762738cb9d7dd3ee477bba389c2f737
Jatin <chaurasiyajatin68@gmail.com>
2024-01-01 14:31:53 +0530

    hello new code

--- COMPUTER PRACTICAL EXPERIMENTS/exp_23.cpp
+++ COMPUTER PRACTICAL EXPERIMENTS/exp_23.cpp
@@ -1,5 +1,32 @@
 //wap to implement virtual function
+#include <iostream>
+using namespace std;
 
+class Base {
+   public:
+    virtual void print() {
+        cout << "Base Function" << endl;
+    }
+};
+
+class Derived : public Base {
+   public:
+    void print() {
+        cout << "Derived Function" << endl;
+    }
+};
+
+int main() {
+    Derived derived1;
+
+    // pointer of Base type that points to derived1
+    Base* base1 = &derived1;
+
+    // calls member function of Derived class
+    base1->print();
+
+    return 0;
+}
 
 
 
