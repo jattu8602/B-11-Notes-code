4eeac43dca361f3cf74163b776ffa16dcda7f5c9
Jatin <chaurasiyajatin68@gmail.com>
2024-01-01 18:07:06 +0530

    hello world

--- COMPUTER PRACTICAL EXPERIMENTS/exp_10.cpp
+++ COMPUTER PRACTICAL EXPERIMENTS/exp_10.cpp
@@ -12,7 +12,7 @@ int main() {
         } 
         cout << endl; 
     } 
-
+cout<<"Matrix is :"<<endl;
     for (int i = 1; i < 4; i++) { 
         for (int j = 1; j < 4; j++) { 
             cout << arr[i][j] << " "; 
